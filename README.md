2257fa4b8f44118a77800e4f16ebd9acd1a575b4
Fletcher <me@fboyd.me>
2023-05-01 14:36:56 +0800

    Initial test

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ha-icon-generator
+
 Generate custom icon packs for Home Assistant from a directory of SVGs
+
+## Usage
+
+`generate.py <svg directory> <icon prefix>`
+
+This will generate `<icon prefix>-icons.json`
---
+++ generate.py
@@ -0,0 +1,88 @@
+# Converts a directory of SVGs to a single JS file suitable for Home Assistant
+
+import os
+import json
+import sys
+
+if len(sys.argv) != 3:
+    print("generate.py <directory> <prefix>")
+    sys.exit(1)
+else:
+    prefix = sys.argv[2]
+    directory = sys.argv[1]
+
+from xml.dom import minidom
+
+# Extract paths from svg
+def extract_data(svg):
+    # Parse the svg
+    doc = minidom.parseString(svg)
+    # Get the path elements
+    paths = doc.getElementsByTagName('path')[0].getAttribute('d')
+
+    # Get the path strings
+    #paths = [path.getAttribute('d') for path in paths]
+    #paths = paths[0].getAttribute('d')
+
+    viewbox = doc.childNodes[0].getAttribute('viewBox')
+    if not viewbox:
+        viewbox = "0 0 24.0 24.0"
+
+    return paths, viewbox
+
+js = """async function getIcon(name) {
+  if (!(name in icons)) {
+    console.log(`Icon "${name}" not available`);
+    return '';
+  }
+
+  var svgDef = icons[name];
+  var primaryPath = svgDef[1];
+  return {
+    path: primaryPath,
+    viewBox: svgDef[0]
+  }
+
+}
+
+async function getIconList() {
+  return Object.entries(icons).map(([icon]) => ({
+    name: icon
+  }));
+}
+
+window.customIconsets = window.customIconsets || {};
+window.customIconsets["PREFIX"] = getIcon;
+
+window.customIcons = window.customIcons || {};
+window.customIcons["PREFIX"] = { getIcon, getIconList };"""
+
+
+
+# Get the path to the svg folder
+svg_folder = os.path.join(os.path.dirname(__file__), directory)
+
+icons = {}
+
+# Open each file in the svg folder
+for filename in os.listdir(svg_folder):
+    # Ignore non-svg files
+    if not filename.endswith('.svg'):
+        continue
+
+    # Open the file
+    with open(os.path.join(svg_folder, filename), 'r') as f:
+        # Read the file
+        svg = f.read()
+
+        # Extract the paths and viewbox
+        data = extract_data(svg)
+        icons[filename.split(".")[0]] = [data[1], data[0]]
+
+s = json.dumps(icons, indent=4)
+
+with open(f"{prefix}-icons.js","w") as f:
+    f.write("var icons = ")
+    f.write(s)
+    f.write("\n")
+    f.write(js.replace("PREFIX",prefix))
